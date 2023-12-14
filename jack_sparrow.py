3438f110880ebf66e95bcb1f6e8f5c0a43f488b5
agprao <108051891+agprao@users.noreply.github.com>
2023-12-14 21:50:33 +0530

    Update jack_sparrow.py

--- jack_sparrow.py
+++ jack_sparrow.py
@@ -1,9 +1,10 @@
 input_string = input('Enter the values of A B C D separated by space \n')
 user_list = input_string.split()
 
-n_boats = 1;
+n_boats = 0;
 for i in range(len(user_list)):
   user_list[i] = int(user_list[i])
   n_boats = n_boats + (user_list[i] * (i + 0))
+n_boats = (n_boats / 4) + (n_boats % 4 > 0)
 
 print("Number of boats = ", n_boats)
